db9ec16390c8a594d2f591eb87987e8732aaf0ec
Jacob Ferrell <michaeljacobferrell@gmail.com>
2026-07-12 20:14:57 -0500

    adds spin and friction for balls

--- src/constants.hpp
+++ src/constants.hpp
@@ -6,6 +6,6 @@ constexpr int WINDOW_WIDTH  = 800;
 constexpr int WINDOW_HEIGHT = 600;
 constexpr Vec2 GRAVITY = {0, 1000};
 constexpr float RESTITUTION_THRESHOLD = 50.0f;
-
+constexpr float EPSILON = 1e-6f ;
 
 
